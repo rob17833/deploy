9f7a3fbc5f78e0dfed43c76d051b2e1091df6b6c
rob17833 <janssensrobert@hotmail.com>
2019-06-19 16:11:37 +0200

    Add seed script

--- data-layer/config/config.js
+++ data-layer/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   use_env_variable: 'DATABASE_URL',
-  seederStorage: 'sequelize', // Keep track of executed seeds to avoid re-execution upon each deployment
+  seederStorage: 'sequelize',  // Keep track of executed seeds to avoid re-execution upon each deployment
   dialectOptions: {
     ssl: process.env.DATABASE_SSL === 'true',
   },
